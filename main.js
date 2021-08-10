16f77d88a367559562a8b94ea53ed0c68d9ce5f4
davidcastel <davidccbp20@gmail.com>
2021-08-09 22:59:41 -0400

    Remove Maze class and imported class

--- main.js
+++ main.js
@@ -52,48 +52,14 @@
  *
  * */
 
-/* This is a class that will create a maze for the user.
- * */
-class Maze {
-  constructor(matrix, start, end) {
-    this.__matrix = matrix;
-    this.__start_pos = start;
-    this.__end_pos = end;
-    this.__maze = this.generateMaze(matrix);
-  }
-
-  // Getters
-  get matrix() {
-    return this.__matrix;
-  }
-
-  get maze() {
-    return this.__maze;
-  }
-
-  // Methods
-  generateMaze(matrix) {
-    let maze = "";
-    for (let i = 0; i < matrix.length; i++) {
-      let rowString = "";
-      let matrixRow = matrix[i];
-
-      for (let j = 0; j < matrixRow.length; j++) {
-        let element = matrixRow[j];
-        if (element === 1) {
-          rowString += "-";
-        } else if (element === 0) {
-          rowString += "*";
-        }
-      }
-
-      maze += rowString += "\n";
-    }
-    return maze;
-  }
-}
+// Imports
+import Maze from "./Maze.js";
 
 const main = function () {
+  // !TODO:
+  // Add function the detemine the type of input by the user
+  // - Binary
+  // - Linear
   const new_maze = new Maze(
     [
       [1, 1],
