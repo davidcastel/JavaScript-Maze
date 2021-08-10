bd9b09137ec37f2cf1fd8ea8baddbf9ffafa0bb9
davidcastel <davidccbp20@gmail.com>
2021-08-09 22:58:37 -0400

    Make Maze.js it's own module

---
+++ Maze.js
@@ -0,0 +1,40 @@
+/* This is a class that will create a maze for the user.
+ * */
+export default class Maze {
+  constructor(matrix, start, end) {
+    this.__matrix = matrix;
+    this.__start_pos = start;
+    this.__end_pos = end;
+    this.__maze = this.generateMaze(matrix);
+  }
+
+  // Getters
+  get matrix() {
+    return this.__matrix;
+  }
+
+  get maze() {
+    return this.__maze;
+  }
+
+  // Methods
+  generateMaze(matrix) {
+    let maze = "";
+    for (let i = 0; i < matrix.length; i++) {
+      let rowString = "";
+      let matrixRow = matrix[i];
+
+      for (let j = 0; j < matrixRow.length; j++) {
+        let element = matrixRow[j];
+        if (element === 1) {
+          rowString += "-";
+        } else if (element === 0) {
+          rowString += "*";
+        }
+      }
+
+      maze += rowString += "\n";
+    }
+    return maze;
+  }
+}
