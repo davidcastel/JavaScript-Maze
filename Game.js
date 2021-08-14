eadc8aff33a763255b135fbbd56486bbba5f0387
davidcastel <davidccbp20@gmail.com>
2021-08-14 16:38:47 -0400

    Create method to take user input

--- Game.js
+++ Game.js
@@ -1,6 +1,24 @@
 import Maze from "./Maze.js";
 import Controller from "./Controller.js";
-
+import readline from 'readline';
 
 export default class Game {
+    constructor() {}
+
+    // Methods
+    start() {
+      const inputMatrix = [[1,1,3], [1,1,0], [2,1,0]];
+      const maze = new Maze(inputMatrix);
+      const controls = new Controller();
+
+      const rl = readline.createInterface({
+          input: process.stdin,
+          output: process.stdout
+      });
+
+      rl.question('Do you want to play the maze game?', answer => {
+          console.log('awesome');
+          rl.close();
+      });
+    }
 }
